f2ad2c8e91f79e287e7192e2eb067367c53a2bec
Jin__Yee <142388279+kjinyeek@users.noreply.github.com>
2024-05-15 23:16:09 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+	int N;
+	scanf("%d", &N);
+	if(N % 4 == 0 && N % 100 != 0){
+		printf("1");
+	}
+	else if(N % 4 == 0 && N % 400 == 0){
+		printf("1");
+	}
+	else{
+		printf("0");
+	}
+	
+	return 0;
+}
