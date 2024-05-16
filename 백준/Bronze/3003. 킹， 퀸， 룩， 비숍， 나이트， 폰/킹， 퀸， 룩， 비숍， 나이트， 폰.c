38c9bcd275d6e4bb2dadd4c09260fe912d93dacc
Jin__Yee <142388279+kjinyeek@users.noreply.github.com>
2024-05-16 21:06:46 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+	int a, b, c, d, e, f;
+	scanf("%d %d %d %d %d %d", &a, &b, &c, &d, &e, &f);
+	printf("%d ", 1 - a);
+	printf("%d ",1 - b);
+	printf("%d ",2 - c);
+	printf("%d ",2-d);
+	printf("%d ",2-e);
+	printf("%d",8-f);
+	
+	return 0;
+}
