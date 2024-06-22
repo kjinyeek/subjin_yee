1e5c934ae27f17b1bf4292a7e9ee5db69f38d8cc
Jin__Yee <142388279+kjinyeek@users.noreply.github.com>
2024-06-23 04:30:15 +0900

    [Bronze V] Title: A×B, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10998. A×B/A×B.cc
+++ 백준/Bronze/10998. A×B/A×B.cc
@@ -1,12 +1,9 @@
 #include <stdio.h>
+
 int main()
 {
-	int N, M;
-	scanf("%d %d", &N, &M);
-	
-	printf("%d", N * M);
-	
-	
-	
-	return 0;
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d", a*b);
+    return 0;
 }
