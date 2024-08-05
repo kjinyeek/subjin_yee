6e7967492b1013aa59d3ca39cd4560d28d10c518
Jin__Yee <142388279+kjinyeek@users.noreply.github.com>
2024-08-05 20:19:22 +0900

    [Silver III] Title: N과 M (3), Time: 428 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+int print[10];
+int N, M;
+void f(int now, int now_cnt)
+{
+	if(now > N || now_cnt > M)return;
+	print[now_cnt] = now;
+	if(now_cnt == M){
+		for(int i = 1; print[i]; i++) printf("%d ", print[i]);
+		printf("\n");
+		print[now_cnt] = 0;
+		return;
+	}
+	
+	
+	for(int i = 1; i <= N; i++){
+		f(i, now_cnt+1);
+	}
+	
+	print[now_cnt] = NULL;
+}
+int main()
+{
+	scanf("%d %d", &N, &M);
+	if(M == 1){
+		for(int i = 1; i <= N; i++)printf("%d\n", i);
+	}
+	for(int i = 1; i <= N; i++){
+		print[1] = i;
+		for(int j = 1; j <= N; j++){
+			f(j, 2);
+		}
+		print[1] = NULL;
+	}
+	
+	return 0;
+}
